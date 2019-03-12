bd6e1f5570452f63526afe4ed1f63927a3553363
ruiterl7 <luca.ruiters@hva.nl>
2019-03-12 23:27:28 +0100

    Initial Commit

---
+++ Multi/Form1.Designer.cs
@@ -0,0 +1,66 @@
+﻿namespace Multi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.lollipopButton1 = new LollipopButton();
+            this.SuspendLayout();
+            // 
+            // lollipopButton1
+            // 
+            this.lollipopButton1.BackColor = System.Drawing.Color.Transparent;
+            this.lollipopButton1.BGColor = "#508ef5";
+            this.lollipopButton1.FontColor = "#ffffff";
+            this.lollipopButton1.Location = new System.Drawing.Point(282, 243);
+            this.lollipopButton1.Name = "lollipopButton1";
+            this.lollipopButton1.Size = new System.Drawing.Size(143, 41);
+            this.lollipopButton1.TabIndex = 0;
+            this.lollipopButton1.Text = "lollipopButton1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lollipopButton1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "HNTR | GUI";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private LollipopButton lollipopButton1;
+    }
+}
+
---
+++ Multi/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Text;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Multi
+{
+    public partial class Form1 : Form
+    {
+        PrivateFontCollection pfc = new PrivateFontCollection();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Load_Data();
+            Set_Data();
+        }
+
+        #region Font/Data-Stuff
+        private void Load_Data()
+        {
+            Load_Font(Path.Combine(Application.StartupPath, "Fonts","OpenSans-Light.ttf")); // #2 - Open Sans Light
+            Load_Font(Path.Combine(Application.StartupPath, "Fonts", "OpenSans-LightItalic.ttf")); // #2.italic - Open Sans Light Italic
+            Load_Font(Path.Combine(Application.StartupPath, "Fonts", "OpenSans-Italic.ttf")); //#0.italic - Open Sans Italic  
+                // #0 (not in the list but available) - Open Sans Regular
+            Load_Font(Path.Combine(Application.StartupPath, "Fonts", "OpenSans-ExtraBold.ttf")); // #1 - Open Sans Extra Bold
+            Load_Font(Path.Combine(Application.StartupPath, "Fonts", "OpenSans-ExtraBoldItalic.ttf")); // #1.italic - Open Sans Extra Bold Italic
+        }
+
+        private void Set_Data()
+        {
+           
+        }
+
+        private void Set_Font(Control control, FontFamily font, int size, FontStyle style)
+        {
+            control.Font = new Font(font, size, style);
+        }
+
+        private void Load_Font(string fontFile)
+        {
+            pfc.AddFontFile(fontFile);
+        }
+
+        #endregion
+    }
+}
